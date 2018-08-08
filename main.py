8bb3b58211af18c183fcce5a21b41982b5519a55
shengyang998 <shengyang998@163.com>
2018-08-08 23:08:11 +0800

    refactor

--- main.py
+++ main.py
@@ -1,31 +1,73 @@
 print("Importing OpenCV")
 import cv2
-print("OpenCV Imported")
+print("OpenCV imported")
 import numpy as np
 import myfunctions as myf
 
-print("Opening Video Capture")
-cap = cv2.VideoCapture(-1)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
-cap.set(cv2.CAP_PROP_FPS, 0.1)
-while(True):
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, -1) # Flip camera vertically
 
-    #cv2.imshow('frame', frame)
+def convert_BGR_to_RGB(BGR_frame):
+    return BGR_frame[:, :, ::-1]
 
-    rgb_frame = frame[:, :, ::-1]
-    face_locations = myf.face_r.face_locations(rgb_frame)
-    face = myf.face_r.face_encodings(rgb_frame, face_locations)
-    myf.is_face_in_white_list(face)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
-        break
+def config_capture():
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640) # set Width
+    cap.set(4,480) # set Height
+    cap.set(cv2.CAP_PROP_FPS, 0.1)
+    return cap
 
-print("Releasing Video Capture")
-cap.release()
-print("Video Capture Released")
-cv2.destroyAllWindows()
 
+def fix_camera_direction(frame):
+    return cv2.flip(frame, 1)
+
+
+def capture_read(cap):
+    _, frame = cap.read()
+    frame = fix_camera_direction(frame)
+    return frame
+
+
+def get_face_locations(frame):
+    return myf.face_r.face_locations(frame)
+
+
+def get_face_encodings(frame):
+    return myf.face_r.face_encodings(frame, get_face_locations(frame))
+
+
+def recognize(frame):
+    rgb_frame = convert_BGR_to_RGB(frame)
+    face = get_face_encodings(rgb_frame)
+    return myf.is_face_in_white_list(face)
+
+
+def capturing(cap):
+    while(True):
+        frame = capture_read(cap)
+        cv2.imshow('frame', frame)
+        recognize(frame)
+
+
+def main():
+    print("Capturing Video")
+    cap = config_capture()
+    try:
+        capturing(cap)
+    except KeyboardInterrupt:
+        print("OK, Retrieving resources before quit")
+        print("Releasing Video Capture")
+        cap.release()
+        print("Video Capture Released")
+        cv2.destroyAllWindows()
+        print("Resources is all retrieved. Will now quit.")
+    except Exception as e:
+        print("An error occured: {0}".format(e))
+        print("Releasing Video Capture")
+        cap.release()
+        print("Video Capture Released")
+        cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    main()
--- myfunctions.py
+++ myfunctions.py
@@ -7,11 +7,13 @@ def load_images(path="."):
     pictures = []
     image_file_names = [ f for f in listdir(path) if f.endswith(".jpg") ]
     for img_name in image_file_names:
-        pictures.append( (img_name, face_r.load_image_file("{0}/{1}".format(path, img_name))) )
+        face_image = face_r.load_image_file("{0}/{1}".format(path, img_name))
+        img_name = " ".join(img_name.split('.')[:-1])
+        pictures.append( (img_name, face_image) )
     return pictures
 
 
-@lru_cache(maxsize=None)
+@lru_cache(maxsize=32)
 def get_whitelist_faces(whitelist_dir="./WhiteList"):
     pictures = load_images(whitelist_dir)
     faces = []
@@ -24,16 +26,21 @@ def get_faces(image):
     return face_r.face_encodings(image)
 
 
+def get_name_from_result(results):
+    return get_whitelist_faces()[results.index(True)][0]
+
+
 def is_face_in_white_list(face):
     if face == None or face == []:
         return
     results = face_r.compare_faces([ f[1] for f in get_whitelist_faces() ], face[0])
     if not True in results:
         print("Attention: face not in white list")
+        return False
     else:
-        print("This is {0}'s face.".format(get_whitelist_faces()[results.index(True)][0]))
-
-
+        print("This is {0}'s face.".format(get_name_from_result(results)))
+        print("Welcome, {0}".format(get_name_from_result(results)))
+        return True
 
 
 
